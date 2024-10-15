c7494653d9644fd89bbc4cc885bab2dcd74042d9
Tianle Wang <twang3@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2024-10-15 07:17:25 +0000

    Reorganize the structure of the repo

--- diffusion_solver/entk/my_run_script.sh
+++
@@ -1,7 +0,0 @@
-python train_net.py --card 0 --initial 1 --portion 1 --epochs 5 --es 1
-
-python active.py --card 0 --portion 1 --func tod
-
-python active.py --card 0 --portion 1 --func random
-
-python train_net.py --card 0 --initial 0 --portion 2 --epochs 5 --es 1
