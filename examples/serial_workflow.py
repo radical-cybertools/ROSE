dba52ec2ec35ab03db6c16c6b3bd6d1ba3d3e6e0
AymenFJA <aymen.alsaadi@rutgers.edu>
2024-10-25 22:48:27 +0000

    refine

--- examples/serial_workflow.py
+++ examples/serial_workflow.py
@@ -1,9 +1,9 @@
 from rose import SerialWorkflow, RoseResource
 from rose import SimulationTask, ActiveLearnTask, TrainingTask
 
-sim_task = SimulationTask(executable='/bin/echo I am simulation task')
-train_task = TrainingTask(executable='/bin/echo I am Training task')
-active_learn_task = ActiveLearnTask(executable='/bin/echo I am AL task')
+sim_task = SimulationTask(executable='/bin/echo I am simulation task $RP_TASK_ID')
+train_task = TrainingTask(executable='/bin/echo I am Training task $RP_TASK_ID')
+active_learn_task = ActiveLearnTask(executable='/bin/echo I am AL task $RP_TASK_ID')
 
 serial_wf = SerialWorkflow(simulation_task=sim_task,
                            training_task=train_task,
