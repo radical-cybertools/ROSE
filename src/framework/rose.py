0b1590baf680a2e3f7076a5639223035ab05169a
AymenFJA <aymen.alsaadi@rutgers.edu>
2024-11-05 22:56:51 +0000

    better example

--- src/framework/rose.py
+++ src/framework/rose.py
@@ -11,8 +11,26 @@ import typeguard
 from data import InputFile, OutputFile
 
 class Task(rp.TaskDescription):
+    """
+    Represents a task description by extending the `TaskDescription` class from `rp` (an external module).
+
+    This class is primarily used to define and manage the details of a task, inheriting properties and methods 
+    from the `rp.TaskDescription` base class. Additional arguments and keyword arguments can be passed to further 
+    configure the task, which are then forwarded to the base class.
+
+    Parameters
+    ----------
+    *args : tuple
+        Positional arguments to pass to the parent class constructor, if needed.
+    **kwargs : dict
+        Keyword arguments to configure the task. Passed directly to the `TaskDescription` initializer.
+
+    Methods
+    -------
+    None. This class relies on inherited methods from `rp.TaskDescription`.
+    """
+
     def __init__(self, *args, **kwargs):
-        kwargs['name'] = 'SimulationTask'
         super().__init__(kwargs)
 
 
