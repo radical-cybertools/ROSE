c9b146153a58cb9a8948754bb3d7b3fc9790b4b5
Mariya Goliyad <mgoliyad@gmail.com>
2025-07-30 13:01:38 -0400

    forgot to add one update

--- rose/uq/__init__.py
+++ rose/uq/__init__.py
@@ -1,3 +1,3 @@
 from rose.uq.UQMetrics import UQMetrics
 #from rose.uq.uq_learner import ParallelUQLearner
-from rose.uq.uq_learner import UQLearner
+from rose.uq.uq_parallel_learner import UQLearner
